9743eb8f1f71c2ae7a17bf2a0b71f273bc45b76e
Retaker <retaker@users.noreply.github.com>
2017-04-22 22:06:06 -0500

    Add files via upload

--- Point.h
+++ Point.h
@@ -11,7 +11,7 @@ struct Point {
 	int x, y;
 	int R, G, B;
 
-	Point(int nx = 0, int ny = 0, int nR = 255, int nG = 255, int nB = 255) {
+	Point(int nx = 0, int ny = 0, int nR = 0, int nG = 0, int nB = 0) {
 		x = nx;
 		y = ny;
 		R = nR;
@@ -19,7 +19,7 @@ struct Point {
 		B = nB;
 	}
 
-	void setColor(int nR = 0, int nG = 0, int nB = 0) {
+	void setColor(int nR = 255, int nG = 255, int nB = 255) {
 		R = nR;
 		G = nG;
 		B = nB;
